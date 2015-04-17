6426dfccf2a6cb64ea1303a06da2ad675a0583f2
Akash Agrawal <akagr@outlook.com>
2015-04-17 16:25:33 +0530

    Added install task for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var fs = require('fs');
 var gutil = require('gulp-util');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
@@ -26,4 +27,34 @@ gulp.task('sass', function(done) {
 
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
+});
+
+/* Installs cordova plugins and jspm packages */
+gulp.task('install', function(done) {
+    if(!sh.which('cordova')) return done(new Error('No \'cordova\' found in path.'));
+    if(!sh.which('jspm')) return done(new Error('No \'jspm\' found in path.'));
+
+    /* Install all cordova plugins */
+    console.log('Installing cordova plugins..');
+    var packageJSON  = require('./package.json');
+    packageJSON.cordovaPlugins.forEach(function(plugin) {
+        var pluginCmd = '';
+        if(typeof plugin === 'string') {
+            pluginCmd += plugin;
+        } else {
+            pluginCmd += plugin.locator + ' ';
+            if(plugin.variables) {
+                Object.keys(plugin.variables).forEach(function(variable){
+                    pluginCmd += '--variable ' + variable + '="' + plugin.variables[variable] + '" ';
+                });
+            }
+        }
+        sh.exec('cordova plugin add ' + pluginCmd);
+    });
+    console.log('Finshed installing cordova plugins.');
+
+    /* Install jspm packages */
+    sh.exec('jspm install');
+
+    done();
 });
